84cec80a41a9b249980f644fc808c7dc003a627e
Muhammadrasul <holturaevm@gmail.com>
2022-11-09 08:31:02 +0500

    this is the first commit

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.8;
+
+contract SimpleProject {
+    // boolean, uint, int, address, bytes
+    bool hasFavoriteNumber = false;
+    uint16 favoriteNum = 123;
+    string favoriteNumInTxt = "Five"; 
+    int256 favoriteInt = -5;
+    address myAddress = 0xe076a7458Eb01D5f421bA3167092fAf5fE5FBb31;
+    bytes32 favoriteBytes = "cat"; //0xq123asd32ad32
+
+}
