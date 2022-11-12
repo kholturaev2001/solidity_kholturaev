5987ff24d6de4e54dca6c5ee35f88b19b5f4af45
Muhammadrasul <holturaevm@gmail.com>
2022-11-12 07:59:56 +0500

    arrays in solidity

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -3,14 +3,16 @@
 pragma solidity ^0.8.6;
 
 contract SimpleProject {
-   uint256 public favoriteNumber;
-
-   People public person = People({favoriteNumber: 2, name: "Muhammadrasul"});
+   uint256 favoriteNumber;
 
    struct People {
        uint256 favoriteNumber;
        string name;
    }
+
+
+    // uint256[] public favoriteNumberList;  
+    People[] public people;
 
    function store(uint256 _favoriteNumber) public  {
        favoriteNumber = _favoriteNumber;
@@ -21,4 +23,17 @@ contract SimpleProject {
    function retrieve() public view returns(uint256) {
        return favoriteNumber;
    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        People memory newPerson = People({favoriteNumber: _favoriteNumber, name: _name});
+        people.push(newPerson);
+    }
+
+
+
+
+
+
 }
+
+
