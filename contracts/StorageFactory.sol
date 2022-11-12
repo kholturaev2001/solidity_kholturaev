398aa9c05c0ab8e54fe007bc0088487d895fa6f2
Muhammadrasul <holturaevm@gmail.com>
2022-11-12 20:54:24 +0500

    interactions with other contracts

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -4,9 +4,25 @@ pragma solidity ^0.8.0;
 import "./SimpleStorage.sol";
 
 contract StorageFactory {
-    SimpleStorage[] public simpleStorageArray; 
+    SimpleStorage[] public simpleStorageArray;
+
     function createSimpleStorageContract() public {
-       SimpleStorage simpleStorage = new SimpleStorage();
-       simpleStorageArray.push(simpleStorage);
+        SimpleStorage simpleStorage = new SimpleStorage();
+        simpleStorageArray.push(simpleStorage);
+    }
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber)
+        public
+    {
+        // Address
+        // ABI - Application Binary Initerface
+
+        SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
+        simpleStorage.store(_simpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns(uint256) {
+        SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
+        return simpleStorage.retrieve();
     }
 }
