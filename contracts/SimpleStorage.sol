36d276f8c0962e94d0573da06b0abdbdac2def7f
Muhammadrasul <holturaevm@gmail.com>
2022-11-12 07:30:59 +0500

    work with structs TWO

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -5,6 +5,13 @@ pragma solidity ^0.8.6;
 contract SimpleProject {
    uint256 public favoriteNumber;
 
+   People public person = People({favoriteNumber: 2, name: "Muhammadrasul"});
+
+   struct People {
+       uint256 favoriteNumber;
+       string name;
+   }
+
    function store(uint256 _favoriteNumber) public  {
        favoriteNumber = _favoriteNumber;
     //    retrieve();
