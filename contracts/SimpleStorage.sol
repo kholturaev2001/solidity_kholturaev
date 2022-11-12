3d5ca8f162d2d6f4a6214451ceeb4871a9e1ec62
Muhammadrasul <holturaevm@gmail.com>
2022-11-12 07:13:52 +0500

    some functions added2

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,14 +1,17 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.8;
+pragma solidity ^0.8.6;
 
 contract SimpleProject {
-    // boolean, uint, int, address, bytes
-    bool hasFavoriteNumber = false;
-    uint16 favoriteNum = 123;
-    string favoriteNumInTxt = "Five"; 
-    int256 favoriteInt = -5;
-    address myAddress = 0xe076a7458Eb01D5f421bA3167092fAf5fE5FBb31;
-    bytes32 favoriteBytes = "cat"; //0xq123asd32ad32
+   uint256 public favoriteNumber;
 
+   function store(uint256 _favoriteNumber) public  {
+       favoriteNumber = _favoriteNumber;
+    //    retrieve();
+   }
+
+   //view, pure
+   function retrieve() public view returns(uint256) {
+       return favoriteNumber;
+   }
 }
