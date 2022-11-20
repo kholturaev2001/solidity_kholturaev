e627f3ad430a928275857df12084404f3b91656f
Muhammadrasul <holturaevm@gmail.com>
2022-11-20 13:13:22 +0500

    SafeMath, Checked & Unchecked

--- contracts/SafeMathTester.sol
+++ contracts/SafeMathTester.sol
@@ -2,10 +2,12 @@
 
 pragma solidity ^0.8.0;
 
-contract SafeMathTester{
-    uint8 public bigNumber = 255; // before, in ^0.6.0 it was unchecked, but now the value of uin8 is checked, and the contract wouldn't compile
+contract SafeMathTester {
+    uint8 public bigNumber = 255; // checked
 
     function add() public {
-        bigNumber = bigNumber + 1;
+        unchecked {
+            bigNumber = bigNumber + 1;
+        }
     }
 }
