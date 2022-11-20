704357a21fd6b933f75ef5e9681c83a2fa4afd59
Muhammadrasul <holturaevm@gmail.com>
2022-11-20 09:20:45 +0500

    Floating Point Math in Solidity and get price of ETH in USD in Goerli Testnet

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -5,49 +5,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-interface AggregatorV3Interface {
-  function decimals() external view returns (uint8);
-
-  function description() external view returns (string memory);
-
-  function version() external view returns (uint256);
-
-  function getRoundData(uint80 _roundId)
-    external
-    view
-    returns (
-      uint80 roundId,
-      int256 answer,
-      uint256 startedAt,
-      uint256 updatedAt,
-      uint80 answeredInRound
-    );
-
-  function latestRoundData()
-    external
-    view
-    returns (
-      uint80 roundId,
-      int256 answer,
-      uint256 startedAt,
-      uint256 updatedAt,
-      uint80 answeredInRound
-    );
-}
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe {
 
-    uint256 public minimumUsd = 50;
+    uint256 public minimumUsd = 50 * 1e18;
 
     function fund() public payable {
         // Want to be able to set a minimum fund amount in USD
         // 1. How do we send ETH to this contract?
-        require(msg.value >= minimumUsd, "Didn't send enough!"); // 1e18 = 1 * 10 ** 18 == 1000000000000000000
+        require(getConversionRate(msg.value) >= minimumUsd, "Didn't send enough!"); // 1e18 = 1 * 10 ** 18 == 1000000000000000000
     }
 
-    function getPrice() public {
+    function getPrice() public view returns(uint256) {
         // ABI
         // Address: 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
+        (, int price,,,) = priceFeed.latestRoundData();
+        // ETH in terms of USD
+        // 1200.00000000
+        return uint256(price * 1e10); // 1**10 == 10000000000
     }
 
     function getVersion() public view returns (uint256) {
@@ -55,7 +32,11 @@ contract FundMe {
         return priceFeed.version();
     }
 
-    function getConversionRate() public {}
+    function getConversionRate(uint256 ethAmount) public view returns(uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
+    }
     
 
     // function withdraw() {}
