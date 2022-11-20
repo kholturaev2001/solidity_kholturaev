5e11751bbe63a69c55579a67d640a24a6d0d3675
Muhammadrasul <holturaevm@gmail.com>
2022-11-20 12:43:56 +0500

    Libraries & minimizinng contract by moving math and priceConversion staff into a PriceConverter.sol library

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -5,39 +5,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "./PriceConverter.sol";
 
 contract FundMe {
+    using PriceConverter for uint256;
 
     uint256 public minimumUsd = 50 * 1e18;
 
+    address[] public funders;
+    mapping(address => uint256) public addressToAmountFunded;
+
     function fund() public payable {
-        // Want to be able to set a minimum fund amount in USD
-        // 1. How do we send ETH to this contract?
-        require(getConversionRate(msg.value) >= minimumUsd, "Didn't send enough!"); // 1e18 = 1 * 10 ** 18 == 1000000000000000000
+        require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enough!"); // 1e18 = 1 * 10 ** 18 == 1000000000000000000
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = msg.value;
     }
 
-    function getPrice() public view returns(uint256) {
-        // ABI
-        // Address: 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
-        (, int price,,,) = priceFeed.latestRoundData();
-        // ETH in terms of USD
-        // 1200.00000000
-        return uint256(price * 1e10); // 1**10 == 10000000000
-    }
-
-    function getVersion() public view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
-        return priceFeed.version();
-    }
-
-    function getConversionRate(uint256 ethAmount) public view returns(uint256) {
-        uint256 ethPrice = getPrice();
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
-        return ethAmountInUsd;
-    }
-    
+   
 
     // function withdraw() {}
 }
