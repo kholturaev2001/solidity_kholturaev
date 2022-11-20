352d50880773e4d39352e642474133e033a0118c
Muhammadrasul <holturaevm@gmail.com>
2022-11-20 14:14:23 +0500

    Basic Solidity - For Loop

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -16,13 +16,23 @@ contract FundMe {
     mapping(address => uint256) public addressToAmountFunded;
 
     function fund() public payable {
-        require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enough!"); // 1e18 = 1 * 10 ** 18 == 1000000000000000000
+        require(
+            msg.value.getConversionRate() >= minimumUsd,
+            "Didn't send enough!"
+        ); // 1e18 = 1 * 10 ** 18 == 1000000000000000000
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] += msg.value;
     }
 
-   
-
-    // function withdraw() {}
+    function withdraw() public {
+        /* starting index, ending index, step amount */
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < funders.length;
+            funderIndex++
+        ) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+    }
 }
-
