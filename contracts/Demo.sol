67addd6ca8f6d0c84de1e87123f17213ebe3ae22
Muhammadrasul <holturaevm@gmail.com>
2022-12-07 08:28:29 +0500

    Work with array and its methods

--- contracts/Demo.sol
+++ contracts/Demo.sol
@@ -4,27 +4,44 @@ pragma solidity ^0.8.0;
 
 contract Demo {
     //Array
-    uint[10] public items;
+    /* Array with dynamic length */
 
-    function demo() public {
-        items[0] = 100;
-        items[1] = 200;
-        items[4] = 400;
+    uint256[] public items;
+    uint256 public len;
+
+    /* SAMPLE WITH PUSH METHOD */
+    function demoPush() public {
+        items.push(4); // method push can not be used in arrays with fixed length
+        items.push(5);
+        len = items.length;
     }
 
-    // Вложенные массивы
-    uint[3][2] public arrInsideArr;
-
-    function demoTwo() public {
-        arrInsideArr = [
-            [2, 54, 1],
-            [6, 23, 77]
-        ];
+    /* ARRAY SAVED IN MEMORY */
+    function sampleMemory() public pure returns(uint256[] memory){
+        uint256[] memory tempArray = new uint256[](10); //set array length while creating an array in memory
+        tempArray[0] = 20;
+        return tempArray;
     }
 
+    // // Вложенные массивы
+    // uint[3][2] public arrInsideArr;
 
+    // function demoTwo() public {
+    //     arrInsideArr = [
+    //         [2, 54, 1],
+    //         [6, 23, 77]
+    //     ];
+    // }
 
-    // // Enum 
+    /* ARRAY WITH FIXED LENGTH */
+    // uint[10] public items;
+    // function demo() public {
+    //     items[0] = 100;
+    //     items[1] = 200;
+    //     items[4] = 400;
+    // }
+
+    // // ENUM
     // enum Status { Paid, Delivered, Recieved}
 
     // Status public currentStatus;
